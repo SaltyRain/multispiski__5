887d0185025fd6bd8b3e9185a7035f7aa0aadcc8
Timur Garipov <tbgaripov@gmail.com>
2019-06-24 16:03:43 +0300

    first

---
+++ multispiski__5/miltiList.cpp
@@ -0,0 +1,9 @@
+//
+//  miltiList.cpp
+//  multispiski__5
+//
+//  Created by Тимур Гарипов on 24/06/2019.
+//  Copyright © 2019 Timur. All rights reserved.
+//
+
+#include "miltiList.hpp"
---
+++ multispiski__5/miltiList.hpp
@@ -0,0 +1,14 @@
+//
+//  miltiList.hpp
+//  multispiski__5
+//
+//  Created by Тимур Гарипов on 24/06/2019.
+//  Copyright © 2019 Timur. All rights reserved.
+//
+
+#ifndef miltiList_hpp
+#define miltiList_hpp
+
+#include <stdio.h>
+
+#endif /* miltiList_hpp */
